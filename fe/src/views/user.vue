3258cead051dcc354e46ef06b620c36988973240
8hal <mkm8421@gmail.com>
2019-03-26 00:11:05 +0900

    api added

---
+++ fe/src/views/user.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>  
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  mounted () {
+    axios.get('http://localhost:3000/api/user')
+      .then((r) => {
+        console.log(r)
+      })
+      .catch((e) => {
+        console.error(e.message)
+      })
+  }  
+}
+</script>
